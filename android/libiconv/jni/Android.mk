c107cd93de5af629bedfadcabb93d38c9137c77f
Antti Peuhkurinen <peuhkura.gmail.com>
2013-08-26 17:17:43 +0300

    Adding generated files

--- android/libiconv/jni/Android.mk
+++ android/libiconv/jni/Android.mk
@@ -2,16 +2,15 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := ../lib/iconv.c \
-../libcharset/lib/localcharset.c \
-../lib/relocatable.c
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../libiconv-1.14/include \
+$(LOCAL_PATH)/../../../libiconv-1.14/libcharset \
+$(LOCAL_PATH)/../../../libiconv-1.14/libcharset/include
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include \
-$(LOCAL_PATH)/../libcharset \
-$(LOCAL_PATH)/../libcharset/include
+LOCAL_SRC_FILES := ../../../libiconv-1.14/lib/iconv.c \
+../../../libiconv-1.14/libcharset/lib/localcharset.c \
+../../../libiconv-1.14/lib/relocatable.c
 
 LOCAL_CFLAGS := -DLIBDIR=\"\" -DANDROID
 LOCAL_MODULE := libiconv
 
 include $(BUILD_SHARED_LIBRARY)
-
